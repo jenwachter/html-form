3c965518ddddd574d40b5c763cd70f48727583e0
Jen Wachter <jwachter@jhu.edu>
2013-02-13 14:36:52 -0500

    The 'label' passed into addButton() is now used as the button's value.

--- src/HtmlForm/Elements/Button.php
+++ src/HtmlForm/Elements/Button.php
@@ -6,19 +6,6 @@ class Button extends Field
 {
 	protected $type = "submit";
 
-	public function __construct($name, $args = array())
-	{
-		$this->name = $name;
-
-		foreach ($args as $k => $v) {
-			if (property_exists($this, $k)) {
-				$this->$k = $v;
-			}
-		}
-
-		$this->compiledAttr = $this->compileAttributes();
-	}
-
 	/**
      * Creates a button form element
      * @param  array $value 	The current value of the form element
@@ -26,6 +13,6 @@ class Button extends Field
      */
 	public function compile($value = "")
 	{
-		return "<input type=\"{$this->type}\" name=\"{$this->name}\" {$this->compiledAttr} value=\"{$this->defaultValue}\" />";
+		return "<input type=\"{$this->type}\" name=\"{$this->name}\" {$this->compiledAttr} value=\"{$this->label}\" />";
 	}
 }
