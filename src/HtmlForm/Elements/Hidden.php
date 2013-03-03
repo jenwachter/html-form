c5bbcf05e6149b9ae0035ae34d112ecd6584ecb6
Jen Wachter <jwachter@jhu.edu>
2013-03-02 19:53:51 -0500

    Removed label from output.

--- src/HtmlForm/Elements/Hidden.php
+++ src/HtmlForm/Elements/Hidden.php
@@ -9,4 +9,15 @@ class Hidden extends Parents\Textbox
 	 * @var string
 	 */
 	protected $type = "hidden";
+
+	/**
+	 * Builds the HTML of the form field.
+	 * 
+	 * @param  string $value Value of the form field
+	 * @return null
+	 */
+	public function compile($value = "")
+	{
+		return "<input type=\"{$this->type}\" name=\"{$this->name}\" {$this->compiledAttr} value=\"{$value}\" />";
+	}
 }
