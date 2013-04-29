a67b0f7163185b9b7ded99109523d005b6ce5589
Jen Wachter <jwachter@jhu.edu>
2013-04-29 14:21:34 -0400

    Fixed tests to pass with changes made to 'option' type form fields.

--- src/HtmlForm/tests/Elements/CheckboxTest.php
+++ src/HtmlForm/tests/Elements/CheckboxTest.php
@@ -20,14 +20,14 @@ class CheckboxTest extends \HtmlForm\tests\Base
 
 	public function testCompileWithNoOptionValue()
 	{
-		$expected = "<label for=\"testField\">Test Field</label><input type=\"checkbox\"  name=\"testField[]\" value=\"test\"  /> test<input type=\"checkbox\"  name=\"testField[]\" value=\"blue\"  /> blue";
+		$expected = "<label for=\"testField\">Test Field</label><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"test\"  /> <span>test</span></div><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"blue\"  /> <span>blue</span></div>";
 		$value = $this->testClass->compile();
 		$this->assertEquals($expected, $value);
 	}
 
 	public function testCompileWithValue()
 	{
-		$expected = "<label for=\"testField\">Test Field</label><input type=\"checkbox\"  name=\"testField[]\" value=\"test\" checked=\"checked\" /> test<input type=\"checkbox\"  name=\"testField[]\" value=\"blue\"  /> blue";
+		$expected = "<label for=\"testField\">Test Field</label><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"test\" checked=\"checked\" /> <span>test</span></div><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"blue\"  /> <span>blue</span></div>";
 		$value = $this->testClass->compile("test");
 		$this->assertEquals($expected, $value);
 	}
@@ -35,7 +35,7 @@ class CheckboxTest extends \HtmlForm\tests\Base
 	public function testCompileWithOptionValue()
 	{
 		$this->setProperty("options", array("one" => "test", "two" => "blue"));
-		$expected = "<label for=\"testField\">Test Field</label><input type=\"checkbox\"  name=\"testField[]\" value=\"one\"  /> test<input type=\"checkbox\"  name=\"testField[]\" value=\"two\"  /> blue";
+		$expected = "<label for=\"testField\">Test Field</label><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"one\"  /> <span>test</span></div><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"two\"  /> <span>blue</span></div>";
 		$value = $this->testClass->compile();
 		$this->assertEquals($expected, $value);
 	}
@@ -43,7 +43,7 @@ class CheckboxTest extends \HtmlForm\tests\Base
 	public function testCompileWithOptionValueWithValue()
 	{
 		$this->setProperty("options", array("one" => "test", "two" => "blue"));
-		$expected = "<label for=\"testField\">Test Field</label><input type=\"checkbox\"  name=\"testField[]\" value=\"one\" checked=\"checked\" /> test<input type=\"checkbox\"  name=\"testField[]\" value=\"two\"  /> blue";
+		$expected = "<label for=\"testField\">Test Field</label><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"one\" checked=\"checked\" /> <span>test</span></div><div class=\"element\"><input type=\"checkbox\"  name=\"testField[]\" value=\"two\"  /> <span>blue</span></div>";
 		$value = $this->testClass->compile("one");
 		$this->assertEquals($expected, $value);
 	}
@@ -52,7 +52,7 @@ class CheckboxTest extends \HtmlForm\tests\Base
 	{
 		$this->setProperty("compiledAttr", "something=\"cool\"");
 
-		$expected = "<label for=\"testField\">Test Field</label><input type=\"checkbox\" something=\"cool\" name=\"testField[]\" value=\"test\"  /> test<input type=\"checkbox\" something=\"cool\" name=\"testField[]\" value=\"blue\"  /> blue";
+		$expected = "<label for=\"testField\">Test Field</label><div class=\"element\"><input type=\"checkbox\" something=\"cool\" name=\"testField[]\" value=\"test\"  /> <span>test</span></div><div class=\"element\"><input type=\"checkbox\" something=\"cool\" name=\"testField[]\" value=\"blue\"  /> <span>blue</span></div>";
 		$value = $this->testClass->compile();
 		$this->assertEquals($expected, $value);
 	}
