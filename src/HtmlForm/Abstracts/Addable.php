ba42022e52d7fc1479eef8fda71fe4e90a71cf1f
Jen Wachter <jwachter@jhu.edu>
2013-05-15 14:40:08 -0400

    Broke out part of __call method and wrote tests for it.

--- src/HtmlForm/Abstracts/Addable.php
+++ src/HtmlForm/Abstracts/Addable.php
@@ -20,18 +20,28 @@ abstract class Addable
 	 */
 	public function __call($method, $args)
 	{
+		if ($className = $this->fincClass($method)) {
+			$reflect  = new \ReflectionClass($className);
+			$element = $reflect->newInstanceArgs($args);
+			$this->elements[] = $element;
+		} else {
+			throw new \Exception("`{$method}()` does not exist on this object.");
+		}
+		return $this;
+	}
+
+	protected function findClass($method)
+	{
 		if (!preg_match("/^add([a-zA-Z]+)/", $method, $matches)) {
 			return false;
 		}
 
 		$className = "\\HtmlForm\\Elements\\{$matches[1]}";
-		
-		if (class_exists($className)) {
-			$reflect  = new \ReflectionClass($className);
-			$element = $reflect->newInstanceArgs($args);
-			
-			$this->elements[] = $element;
+
+		if (!class_exists($className)) {
+			return false;
 		}
-		return $this;
+
+		return $className;
 	}
 }
