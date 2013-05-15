ff129151e8e07da6f2d1e96c28247d8b18496ed8
Jen Wachter <jwachter@jhu.edu>
2013-05-15 08:09:52 -0400

    Fixed renderElements() to make sure 'addable' var is an addable.

--- src/HtmlForm/Form.php
+++ src/HtmlForm/Form.php
@@ -265,7 +265,7 @@ class Form extends Abstracts\Addable
 	 */
 	protected function renderElements($addable)
 	{
-		if (!in_array("HtmlForm\Abstracts\Addable", class_parents($addable))) {
+		if (!is_object($addable) || is_object($addable) && !in_array("HtmlForm\Abstracts\Addable", class_parents($addable))) {
 			return;
 		}
 
