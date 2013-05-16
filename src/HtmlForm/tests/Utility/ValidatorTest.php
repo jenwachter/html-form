4b6bc1924aeb1e871b75562492f9aec0db184fa5
Jen Wachter <jwachter@jhu.edu>
2013-05-16 13:05:30 -0400

    Wrote tests for number()

--- src/HtmlForm/tests/Utility/ValidatorTest.php
+++ src/HtmlForm/tests/Utility/ValidatorTest.php
@@ -34,17 +34,34 @@ class ValidatorTest extends \HtmlForm\tests\Base
 
 	public function testRequired()
 	{
-		// no value
 		$method = $this->getMethod("required");
+
+		// no value
 		$result = $method->invoke($this->testClass, "Field", "", "");
 		$this->assertEquals(false, $result);
-
 		$errorArray = array("Field is a required field.");
 		$this->assertEquals($errorArray, $this->getProperty("errors"));
 
 		// value
-		$method = $this->getMethod("required");
 		$result = $method->invoke($this->testClass, "Field", "Value", "");
+		$this->assertEquals(true, $result);
+	}
+
+	public function testNumber()
+	{
+		$method = $this->getMethod("number");
+
+		// not a number
+		$result = $method->invoke($this->testClass, "Field", "three", "");
+		$this->assertEquals(false, $result);
+		$errorArray = array("Field must be a number.");
+		$this->assertEquals($errorArray, $this->getProperty("errors"));
+
+		// number
+		$result = $method->invoke($this->testClass, "Field", 3, "");
+		$this->assertEquals(true, $result);
+
+		$result = $method->invoke($this->testClass, "Field", "3", "");
 		$this->assertEquals(true, $result);
 	}
 }
