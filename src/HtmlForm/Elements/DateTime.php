5dfd3416743942772dcffbfa834b32af8038bb11
Jen Wachter <jenleighkelly@gmail.com>
2016-02-29 10:32:12 -0500

    Added date, datetime, and time form fields

---
+++ src/HtmlForm/Elements/DateTime.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HtmlForm\Elements;
+
+class DateTime extends Parents\Textbox
+{
+	/**
+	 * Value of the "type" attribute of this form element
+	 * @var string
+	 */
+	protected $type = "datetime-local";
+}
