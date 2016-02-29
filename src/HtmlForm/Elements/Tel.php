4003d4b7395993e19d60def0d8108bef5be9ff15
Jen Wachter <jenleighkelly@gmail.com>
2016-02-29 11:34:11 -0500

    Added Tel textbox type.

---
+++ src/HtmlForm/Elements/Tel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HtmlForm\Elements;
+
+class Tel extends Parents\Textbox
+{
+	/**
+	 * Value of the "type" attribute of this form element
+	 * @var string
+	 */
+	protected $type = "tel";
+}
